839d81254e8f6895af09623040a5ff19e3aeb2af
dinyardin <dinyardin@gmail.com>
2021-07-14 14:36:27 -0400

    Rendered day 1 project on Preview component

---
+++ src/components/Preview/Preview.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Project1 from '../Projects/project1/Project1'
+
+function Preview() {
+    return (
+        <div>
+            <Project1 />
+        </div>
+    )
+}
+
+export default Preview
