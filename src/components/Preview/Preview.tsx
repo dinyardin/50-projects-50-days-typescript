214cfbf71f97196141e3e45be95694e1f1c8df0d
dinyardin <dinyardin@gmail.com>
2021-07-14 15:08:59 -0400

    Added basic projects switching functionslity

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 import Project1 from '../Projects/project1/Project1'
+import Project2 from '../Projects/project2/Project2'
 
 function Preview() {
+    const [projectNumber, setProjectNumber] = useState<number>(0)
+    const [projects, setProjects] = useState<any[]>([
+        <Project1 />,
+        <Project2 />,
+    ])
+
+    const incrementProjectNumber = () => {
+        setProjectNumber((prev) => prev + 1)
+    }
+
+    const decrementProjectNumber = () => {
+        setProjectNumber((prev) => prev - 1)
+    }
+
     return (
         <div>
-            <Project1 />
+            {projects[projectNumber]}
+            <button onClick={incrementProjectNumber}>Next</button>
+            <button onClick={decrementProjectNumber}>Back</button>
         </div>
     )
 }
